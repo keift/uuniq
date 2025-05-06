64e348c566933c4b0466fe8e4284f01ffb24025c
фырат ёздэн <31664778+fir4tozden@users.noreply.github.com>
2025-05-06 08:41:06 +0300

    Create main.ts

---
+++ src/types/main.ts
@@ -0,0 +1,7 @@
+export * from "./Limits";
+export * from "./Parts";
+export * from "./Shifts";
+export * from "./SnowflakeOptions";
+export * from "./SnowflakeResolve";
+export * from "./SymbolicOptions";
+export * from "./SymbolicResolve";
