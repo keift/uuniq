93d57a5b7e0533fb702faceaf7ec4f177e7f4ac3
фырат ёздэн <31664778+fir4tozden@users.noreply.github.com>
2025-05-06 08:41:55 +0300

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,11 @@
 import Snowflake from "./Snowflake";
 import Symbolic from "./Symbolic";
+import Types from "./types/main";
 
 const Uuniq = {
   Snowflake,
-  Symbolic
+  Symbolic,
+  Types
 };
 
 export default Uuniq;
