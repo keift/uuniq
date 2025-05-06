58e3cf1d61893bf83e7d308be0c240c220a3d939
фырат ёздэн <31664778+fir4tozden@users.noreply.github.com>
2025-05-06 09:42:43 +0300

    Update main.ts

--- src/types/main.ts
+++ src/types/main.ts
@@ -1,7 +1,7 @@
-export { default as Limits } from "./Limits";
-export { default as Parts } from "./Parts";
-export { default as Shifts } from "./Shifts";
-export { default as SnowflakeOptions } from "./SnowflakeOptions";
-export { default as SnowflakeResolve } from "./SnowflakeResolve";
-export { default as SymbolicOptions } from "./SymbolicOptions";
-export { default as SymbolicResolve } from "./SymbolicResolve";
+export type { Limits } from "./Limits";
+export type { Parts } from "./Parts";
+export type { Shifts } from "./Shifts";
+export type { SnowflakeOptions } from "./SnowflakeOptions";
+export type { SnowflakeResolve } from "./SnowflakeResolve";
+export type { SymbolicOptions } from "./SymbolicOptions";
+export type { SymbolicResolve } from "./SymbolicResolve";
