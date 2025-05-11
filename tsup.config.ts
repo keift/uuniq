5408353f6378bce2db62aa052ad122533052dd03
Fırat Özden <fir4tozden@gmail.com>
2025-05-11 05:02:02 +0300

    v1.2.8

--- tsup.config.ts
+++ tsup.config.ts
@@ -1,9 +1,9 @@
 import { defineConfig } from "tsup";
 
 export default defineConfig({
+  format: ["esm", "cjs"],
   entry: ["./src/main.ts"],
   outDir: "./dist",
-  format: ["esm", "cjs"],
   splitting: true,
   clean: true,
   dts: true
