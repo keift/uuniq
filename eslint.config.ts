0f2ef52ac3296b201dc1f4d1ae16a2e90d68a57a
фырат ёздэн <31664778+fir4tozden@users.noreply.github.com>
2025-05-18 16:12:38 +0300

    Update eslint.config.ts

--- eslint.config.ts
+++ eslint.config.ts
@@ -43,6 +43,7 @@ export default [
       "typescript-eslint/consistent-type-exports": ["error", { fixMixedExportsWithInlineTypeSpecifier: true }],
       "typescript-eslint/consistent-type-imports": ["error", { prefer: "type-imports" }],
       "typescript-eslint/explicit-function-return-type": "error",
+      "typescript-eslint/explicit-member-accessibility": ["error", { accessibility: "explicit" }],
       "typescript-eslint/prefer-readonly": "error",
       "typescript-eslint/typedef": [
         "error",
@@ -60,3 +61,4 @@ export default [
     }
   }
 ];
+
