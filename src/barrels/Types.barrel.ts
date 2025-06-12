2120d4c8eefdc76226cfb5288ffb3543509f1d24
Fırat Özden <fir4tozden@gmail.com>
2025-06-12 18:48:16 +0300

    v1.2.16

---
+++ src/barrels/Types.barrel.ts
@@ -0,0 +1,4 @@
+export type { SnowflakeOptions } from "../types/SnowflakeOptions.type";
+export type { SnowflakeResolve } from "../types/SnowflakeResolve.type";
+export type { SymbolicOptions } from "../types/SymbolicOptions.type";
+export type { SymbolicResolve } from "../types/SymbolicResolve.type";
--- src/types/main.type.ts
+++
@@ -1,4 +0,0 @@
-export type { SnowflakeOptions } from "./SnowflakeOptions.type";
-export type { SnowflakeResolve } from "../types/SnowflakeResolve.type";
-export type { SymbolicOptions } from "./SymbolicOptions.type";
-export type { SymbolicResolve } from "../types/SymbolicResolve.type";
