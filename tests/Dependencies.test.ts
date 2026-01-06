ae06096f3e0a0365a0bd4b4023074ba07d437b56
fir4tozden <fir4tozden@gmail.com>
2026-01-06 23:42:09 +0300

    v1.3.15

--- tests/Dependencies.test.ts
+++ tests/Dependencies.test.ts
@@ -2,8 +2,8 @@ import Package from '../package.json';
 
 if (Package.devDependencies['@keyv/mongo'] !== 'latest') throw new Error('❌ Error: @keyv/mongo');
 if (Package.devDependencies.keyv !== 'latest') throw new Error('❌ Error: keyv');
-if (Package.devDependencies.neatlint !== 'latest') throw new Error('❌ Error: neatlint');
 if (Package.devDependencies.prettier !== 'latest') throw new Error('❌ Error: prettier');
+if (Package.devDependencies.rulint !== 'latest') throw new Error('❌ Error: rulint');
 if (Package.devDependencies.tsup !== 'latest') throw new Error('❌ Error: tsup');
 
 console.log('✅ Success');
