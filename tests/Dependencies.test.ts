87613c13d02cc861465a9f8569d5e30bedc1f5c5
fir4tozden <fir4tozden@gmail.com>
2026-04-17 05:28:26 +0300

    v1.3.22

--- tests/Dependencies.test.ts
+++ tests/Dependencies.test.ts
@@ -4,6 +4,6 @@ if (Package.devDependencies['@keyv/mongo'] !== 'latest') throw new Error('❌ Er
 if (Package.devDependencies.keyv !== 'latest') throw new Error('❌ Error: keyv');
 if (Package.devDependencies.prettier !== 'latest') throw new Error('❌ Error: prettier');
 if (Package.devDependencies.rulint !== 'latest') throw new Error('❌ Error: rulint');
-if (Package.devDependencies.tsup !== 'latest') throw new Error('❌ Error: tsup');
+if (Package.devDependencies.tsdown !== 'latest') throw new Error('❌ Error: tsdown');
 
 console.log('✅ Success');
